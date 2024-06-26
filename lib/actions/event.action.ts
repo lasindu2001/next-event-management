b61f8ec6e14ca859287a86f4918935b8182fb5f6
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-06-26 20:50:09 +0530

    get one event server action

---
+++ lib/actions/event.action.ts
@@ -0,0 +1,23 @@
+import { connectToDatabase } from "../database"
+import Category from "../database/models/category.model"
+import Event from "../database/models/event.model"
+import User from "../database/models/user.model"
+import { handleError } from "../utils"
+
+const populateEvent = (query: any) => {
+    return query
+        .populate({ path: 'organizer', model: User, select: '_id firstName lastName' })
+        .populate({ path: 'category', model: Category, select: '_id name' })
+}
+
+// GET ONE EVENT BY ID
+export async function getEventById(eventId: string) {
+    try {
+        await connectToDatabase()
+        const event = await populateEvent(Event.findById(eventId))
+        if (!event) throw new Error('Event not found')
+        return JSON.parse(JSON.stringify(event))
+    } catch (error) {
+        handleError(error)
+    }
+}
