fee4478fbb1c10736e628a9eed9d4c7b329600fe
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-06-26 14:39:20 +0530

    delete user server action

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -4,6 +4,9 @@ import { CreateUserParams, UpdateUserParams } from "@/types"
 import { connectToDatabase } from "../database"
 import User from "../database/models/user.model"
 import { handleError } from "../utils"
+import Event from "../database/models/event.model"
+import Order from "../database/models/order.model"
+import { revalidatePath } from "next/cache"
 
 export async function createUser(user: CreateUserParams) {
     try {
@@ -17,11 +20,33 @@ export async function createUser(user: CreateUserParams) {
 
 export async function updateUser(clerkId: string, user: UpdateUserParams) {
     try {
-      await connectToDatabase()
-      const updatedUser = await User.findOneAndUpdate({ clerkId }, user, { new: true })
-      if (!updatedUser) throw new Error('User update failed')
-      return JSON.parse(JSON.stringify(updatedUser))
+        await connectToDatabase()
+        const updatedUser = await User.findOneAndUpdate({ clerkId }, user, { new: true })
+        if (!updatedUser) throw new Error('User update failed')
+        return JSON.parse(JSON.stringify(updatedUser))
     } catch (error) {
-      handleError(error)
+        handleError(error)
     }
-  }
+}
+
+export async function deleteUser(clerkId: string) {
+    try {
+        await connectToDatabase()
+        const userToDelete = await User.findOne({ clerkId })
+        if (!userToDelete) {
+            throw new Error('User not found')
+        }
+        await Promise.all([
+            Event.updateMany(
+                { _id: { $in: userToDelete.events } },
+                { $pull: { organizer: userToDelete._id } }
+            ),
+            Order.updateMany({ _id: { $in: userToDelete.orders } }, { $unset: { buyer: 1 } }),
+        ])
+        const deletedUser = await User.findByIdAndDelete(userToDelete._id)
+        revalidatePath('/')
+        return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null
+    } catch (error) {
+        handleError(error)
+    }
+}
